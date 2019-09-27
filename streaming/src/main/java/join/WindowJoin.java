900464a21133ca203f83d10712bd81ab5901c991
xinze <1050748528@qq.com>
2019-09-27 14:05:43 +0800

    flink使用07-通过join合并流的操作

---
+++ streaming/src/main/java/join/WindowJoin.java
@@ -0,0 +1,66 @@
+package join;
+
+import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+public class WindowJoin {
+    public static void main(String[] args) throws Exception {
+
+        final long windowSize = 200L;
+        final long rate = 3L;
+
+        System.out.println("Using windowSize=" + windowSize + ", data rate=" + rate);
+        System.out.println("To customize example, use: WindowJoin [--windowSize <window-size-in-millis>] [--rate <elements-per-second>]");
+
+        // 获取env, 配置为"ingestion time"
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime);
+
+
+        // 生成 grade 和 salary 两个流 分别是 (name, grade) / (name, salary)
+        DataStream<Tuple2<String, Integer>> grades = WindowJoinSampleData.GradeSource.getSource(env, rate);
+        DataStream<Tuple2<String, Integer>> salaries = WindowJoinSampleData.SalarySource.getSource(env, rate);
+
+        DataStream<Tuple3<String, Integer, Integer>> joinedStream = runWindowJoin(grades, salaries, windowSize);
+
+        joinedStream.print().setParallelism(1);
+
+        env.execute("Windowed Join Example");
+    }
+
+    public static DataStream<Tuple3<String, Integer, Integer>> runWindowJoin(
+            DataStream<Tuple2<String, Integer>> grades,
+            DataStream<Tuple2<String, Integer>> salaries,
+            long windowSize) {
+
+        return grades.join(salaries)
+                .where(new NameKeySelector())
+                .equalTo(new NameKeySelector())
+
+                .window(TumblingEventTimeWindows.of(Time.milliseconds(windowSize)))
+
+                .apply(new JoinFunction<Tuple2<String, Integer>, Tuple2<String, Integer>, Tuple3<String, Integer, Integer>>() {
+
+                    @Override
+                    public Tuple3<String, Integer, Integer> join(
+                            Tuple2<String, Integer> first,
+                            Tuple2<String, Integer> second) {
+                        return new Tuple3<String, Integer, Integer>(first.f0, first.f1, second.f1);
+                    }
+                });
+    }
+
+    private static class NameKeySelector implements KeySelector<Tuple2<String, Integer>, String> {
+        @Override
+        public String getKey(Tuple2<String, Integer> value) {
+            return value.f0;
+        }
+    }
+}
