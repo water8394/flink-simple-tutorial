b1916a7bb0b2ec2f7857abefeb3093541d70644d
xinze <1050748528@qq.com>
2019-10-08 21:14:42 +0800

    flink使用12-用-table-API-实现WordCount

---
+++ table/src/main/java/wordCount/WordCountTable.java
@@ -0,0 +1,60 @@
+package wordCount;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.BatchTableEnvironment;
+
+
+public class WordCountTable {
+
+    // *************************************************************************
+    //     PROGRAM
+    // *************************************************************************
+
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        BatchTableEnvironment tEnv = BatchTableEnvironment.create(env);
+
+        DataSet<WC> input = env.fromElements(
+                new WC("Hello", 1),
+                new WC("flink", 1),
+                new WC("Hello", 1));
+
+        Table table = tEnv.fromDataSet(input);
+
+        Table filtered = table
+                .groupBy("word")
+                .select("word, frequency.sum as frequency")
+                .filter("frequency = 2");
+
+        DataSet<WC> result = tEnv.toDataSet(filtered, WC.class);
+
+        result.print();
+    }
+
+    // *************************************************************************
+    //     USER DATA TYPES
+    // *************************************************************************
+
+    /**
+     * POJO  word
+     */
+    public static class WC {
+        public String word;
+        public long frequency;
+
+        // public constructor to make it a Flink POJO
+        public WC() {}
+
+        public WC(String word, long frequency) {
+            this.word = word;
+            this.frequency = frequency;
+        }
+
+        @Override
+        public String toString() {
+            return "word: " + word + " | num: " + frequency;
+        }
+    }
+}
